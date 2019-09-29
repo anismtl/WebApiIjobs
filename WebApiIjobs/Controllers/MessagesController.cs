e03b8d9947b088e88eb300a3511c9d4bed9db257
judithadame <judith.adame@gmail.com>
2019-09-29 16:35:22 -0400

    ajout de la methode public List<Message> GetMessageByDateHrPeriode(DateTime dateRappel, int periode) dans la classe MessagesController

--- WebApiIjobs/Controllers/MessagesController.cs
+++ WebApiIjobs/Controllers/MessagesController.cs
@@ -19,6 +19,41 @@ namespace WebApiIjobs.Controllers
             _context = context;
         }
 
-
+        // GET: api/Message
+        [HttpGet("Rappels/{dateRappel}/{periode}")]
+        public List<Message> GetMessageByDateHrPeriode(DateTime dateRappel, int periode)
+        {
+            var message =       (from ra in _context.Rappel
+                                 join ev in _context.Evenement on ra.IdEvenement equals ev.IdEvenement into ra_ev
+                                 from raev in ra_ev.DefaultIfEmpty()
+                                 join ca in _context.Candidat on raev.IdCandidat equals ca.IdCandidat into ca_raev
+                                 from caraev in ca_raev.DefaultIfEmpty()
+                                 //join co in _context.Contact on raev.IdContact equals co.IdContact into co_raev
+                                 //from coraev in ca_raev.DefaultIfEmpty()
+                                 where ((ra.DateRappel >= dateRappel) &&
+                                        (ra.DateRappel <= dateRappel.AddMinutes(periode)))
+                                 select new Message()
+                                 {
+                                     TitreEvent = raev.Titre,
+                                     DateEvent = raev.DateEvent,
+                                     HeureEvent = raev.Heure,
+                                     AdresseEvent = raev.Adresse,
+                                     DescrEvent = raev.Descr,
+                                     DateRappel = ra.DateRappel,
+                                     HeureRappel = ra.HeureRappel,
+                                     TelRappel = ra.TelRappel,
+                                     CourrielRappel = ra.CourrielRappel,
+                                     NomCandidat = caraev.NomCandidat,
+                                     PrenomCandidat = caraev.PrenomCandidat,
+                                     //TitreOffre = of.TitreOffre,
+                                     //CompanieOffre = of.CompanieOffre,
+                                     //NomContact = co.NomContact,
+                                     //PrenomContact = co.PrenomContact,
+                                     //CourrielContact = co.CourrielContact,
+                                     //Poste = co.Poste,
+                                     //TelContact = co.TelContact
+                                 });
+            return message.ToList();
+        }
     }
 }
