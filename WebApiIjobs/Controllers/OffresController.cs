455d7fdf5be3f00e77fecbe47fc6dfdfb5f167e1
Yuniesky Mendez Rodriguez <1895543@bdeb.qc.ca>
2019-10-04 16:32:32 -0400

    Ajoute dans la class Startup inyection de dependence pour regle le probleme de 'Access-Control-Allow-Origin'

--- WebApiIjobs/Controllers/OffresController.cs
+++ WebApiIjobs/Controllers/OffresController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Http.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ namespace WebApiIjobs.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCorsAttribute("http://localhost:65251", "*", "*")]
     public class OffresController : ControllerBase
     {
         private readonly ijobsContext _context;
--- WebApiIjobs/Startup.cs
+++ WebApiIjobs/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +29,14 @@ namespace WebApiIjobs
         {
             services.AddDbContext<ijobsContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //Enable Origin from WebApi 
+            services.AddCors(options => options.AddPolicy("EnableCORS", builder => 
+            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials().Build()));
+            
+            
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -38,6 +46,7 @@ namespace WebApiIjobs
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors("EnableCORS");
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseHttpsRedirection();
